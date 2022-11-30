54b6272e5296259580a27c40a99b0be1b7ecf104
Dulguun <Dulguunn0127@gmail.com>
2022-11-30 18:08:38 +0800

    hi

---
+++ bootstrap/javascritp/babby-shark.js
@@ -0,0 +1,49 @@
+        let  shark = "Babyshark";
+        let doo = "doo-doo" ;
+        let shark1 = "Mommyshark" ;
+        let shark2 = "Daddyshark" ;
+        let shark3 = "Grandpashark";
+        let shark4 = "Let's go hunt";
+        let shark5 = "Run away";
+        let shark6 = "Run away (ah!)";
+        let shark7 ="Safe at last";
+        let shark8 ="Safe at last (phew)";
+        let shark9 = "it's the end";
+       
+
+
+
+        console.log(shark+ ", "+doo+ " "+doo);
+        console.log(shark+ ", "+doo+ " "+doo);
+        console.log(shark+ ", "+doo+ " "+doo);
+        console.log (shark)
+        console.log (shark1 + ","+doo+" "+doo)
+        console.log (shark1 + ","+doo+" "+doo)
+        console.log (shark1 + ","+doo+" "+doo)
+        console.log(shark1)
+        console.log (shark2 + " ,"+doo+","+doo)
+        console.log (shark2 + " ,"+doo+","+doo)
+        console.log (shark2 + " ,"+doo+","+doo)
+        console.log(shark2)
+        console.log (shark3 + " ,"+doo+","+doo)
+        console.log (shark3 + " ,"+doo+","+doo)
+        console.log (shark3 + " ,"+doo+","+doo)
+        console.log(shark3)
+        console.log (shark4 + " ,"+doo+","+doo)
+        console.log (shark4 + " ,"+doo+","+doo)
+        console.log (shark4 + " ,"+doo+","+doo)
+        console.log(shark4)
+        console.log (shark5 + " ,"+doo+","+doo)
+        console.log (shark5 + " ,"+doo+","+doo)
+        console.log (shark5 + " ,"+doo+","+doo)
+        console.log(shark6)
+        console.log (shark7 + " ,"+doo+","+doo)
+        console.log (shark7 + " ,"+doo+","+doo)
+        console.log (shark7 + " ,"+doo+","+doo)
+        console.log(shark8)
+        console.log (shark9 + " ,"+doo+","+doo)
+        console.log (shark9 + " ,"+doo+","+doo)
+        console.log (shark9 + " ,"+doo+","+doo)
+        console.log(shark9)
+
+        
